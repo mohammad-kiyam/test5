c845b19e1a0507414887af94b25d02292f2a3c0c
André Henry <awh9@njit.edu>
2024-09-29 05:14:13 +0000

    adding registration html code

--- frontend/app.py
+++ frontend/app.py
@@ -16,17 +16,18 @@ def registration():
         email = request.form['email']
         first_name = request.form['first_name']
         last_name = request.form['last_name']
-        street = request.form['street']
+        address= request.form['address']
         city = request.form['city']
         state = request.form['state']
         zip = request.form['zip']
         recent_job = request.form['job']
         school  = request.form['school']
+        major = request.form['major']
         start_year = request.form['start_year']
         end_year = request.form['end_year']
         job_title = request.form['job_title']
         employment_type = request.form['employment_type']
-        company_name = request.form['company_name']
+        company_name = request.form['company']
 
         return redirect('/register')
     
