f1ee325a8f83281aa69058250b3e35fefb8fd879
mohammad-kiyam <mk42@njit.edu>
2024-09-29 19:03:41 +0000

    made file creation more dynamic

--- scripts/run_php_apache.sh
+++ scripts/run_php_apache.sh
@@ -4,6 +4,7 @@
 if ! command -v apache2 &> /dev/null; then
     echo "Apache is not installed. Installing Apache..."
     sudo apt update
+    sudo apt upgrade -y
     sudo apt install apache2 -y
 else
     echo "Apache is already installed."
@@ -12,7 +13,6 @@ fi
 # installs php if not installed
 if ! command -v php &> /dev/null; then
     echo "PHP is not installed. Installing PHP..."
-    sudo apt update
     sudo apt install php libapache2-mod-php -y
 else
     echo "PHP is already installed."
@@ -29,9 +29,9 @@ else
 fi
 
 # Create a basic index.php file if it doesn't exist but need to find out how to make the directory consistent with everyone else's VM
-if [ ! -f "/home/moe/Capstone-Group-03/backend/index.php" ]; then
+if [ ! -f "~/Capstone-Group-03/backend/index.php" ]; then
     echo "Creating index.php..."
-    sudo bash -c 'cat <<EOF > /home/moe/Capstone-Group-03/backend/index.php
+    sudo bash -c 'cat <<EOF > ~/Capstone-Group-03/backend/index.php
 <?php
     echo "Hello, World!";
 ?>
