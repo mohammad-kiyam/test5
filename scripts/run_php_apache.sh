83cdc0e4c9a9c036a463396bf34860175ce420e5
mohammad-kiyam <mk42@njit.edu>
2024-09-29 00:35:02 +0000

    backend 1 + restructured directories

---
+++ scripts/run_php_apache.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# installs apache if not installed
+if ! command -v apache2 &> /dev/null; then
+    echo "Apache is not installed. Installing Apache..."
+    sudo apt update
+    sudo apt install apache2 -y
+else
+    echo "Apache is already installed."
+fi
+
+# installs php if not installed
+if ! command -v php &> /dev/null; then
+    echo "PHP is not installed. Installing PHP..."
+    sudo apt update
+    sudo apt install php libapache2-mod-php -y
+else
+    echo "PHP is already installed."
+fi
+
+# checks the status, if not active then starts server
+apache_status=$(sudo systemctl is-active apache2)
+
+if [ "$apache_status" == "active" ]; then
+    echo "Apache is running."
+else
+    echo "Apache is not running. Starting Apache service..."
+    sudo systemctl start apache2
+fi
+
+# Create a basic index.php file if it doesn't exist
+if [ ! -f "/home/moe/Capstone-Group-03/backend/index.php" ]; then
+    echo "Creating index.php..."
+    sudo bash -c 'cat <<EOF > /home/moe/Capstone-Group-03/backend/index.php
+<?php
+    echo "Hello, World!";
+?>
+EOF'
+    echo "index.php created"
+else
+    echo "index.php already exists"
+fi
+
+echo "The PHP backend is running on Apache."
