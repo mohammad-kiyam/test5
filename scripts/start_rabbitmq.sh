16aac9f69fe4c7a71e048c7227ab33fd4b4fe2f5
Nadia <nm94@njit.edu>
2024-10-07 01:17:25 +0000

    RabbitMQ startup script with installation and erlang

--- scripts/start_rabbitmq.sh
+++ scripts/start_rabbitmq.sh
@@ -38,3 +38,43 @@ sudo systemctl enable rabbitmq-server
 sudo systemctl start rabbitmq-server
 
 echo "RabbitMQ service has been enabled and started, run sudo systemctl status rabbitmq-server to check status"
+#!/bin/bash
+
+#Code checks if Erlang is installed
+if dpkg -l | grep -q erlang; then
+    echo "Erlang already installed"
+else
+    echo "Erlang not installed. Installing Erlang now.."
+
+    #update package list
+    sudo apt-get update
+
+    #install erlang
+    sudo apt-get install -y erlang
+
+    #message if successful
+    echo "Erlang has been successfully installed"
+fi
+
+#Check to see if RabbitMQ is installed
+if dpkg -l | grep -q rabbitmq-server; then
+    echo "RabbitMQ already installed"
+else
+    echo "RabbitMQ not installed. Installing RabbitMQ now.."
+    
+    #update package list
+    sudo apt-get update
+
+    #install rabbitmq
+    sudo apt-get install -y rabbitmq-server
+
+    echo "RabbitMQ has been successfully installed"
+fi
+
+#Enable RabbitMQ to start on boot
+sudo systemctl enable rabbitmq-server
+
+#Start rabbitmq
+sudo systemctl start rabbitmq-server
+
+echo "RabbitMQ service has been enabled and started, run sudo systemctl status rabbitmq-server to check status"
