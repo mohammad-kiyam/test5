acbd7f9111f431580bc622b3c8f8aa7e7ab1de7a
mohammad-kiyam <mk42@njit.edu>
2024-10-08 05:07:06 +0000

    testing connection between frontend and backend through rabbitmq

--- backend/register.php
+++ backend/register.php
@@ -7,8 +7,8 @@ use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 
 try {
-    // Establish RabbitMQ connection
-    $rabbitMQConnection = new AMQPStreamConnection('10.147.17.228', 5672, 'guest', 'guest'); // Ensure correct IP
+    // Establishing RabbitMQ connection
+    $rabbitMQConnection = new AMQPStreamConnection('10.147.17.228', 5672, 'guest', 'guest');
     $channel = $rabbitMQConnection->channel();
 
     // Declare the queue to listen to (same queue name in Flask)
