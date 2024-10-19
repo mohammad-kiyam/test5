dd8e49c553b6f882dea7b421b75eb9c863712a46
mohammad-kiyam <mk42@njit.edu>
2024-10-19 14:50:37 +0000

    backups work during login process

--- backend2/login2.php
+++ backend2/login2.php
@@ -16,6 +16,12 @@ try {
     // Declare the queue to send login responses to app.py
     $channel->queue_declare('login_response_queue', false, true, false, false);
 
+    // Declare the queue for listening to login request (backup for backend1)
+    $channel->queue_declare('mysql_login_request_queue', false, true, false, false);
+
+    // Declare the queue for forwarding login responses to mysql login request (backup for backend1)
+    $channel->queue_declare('login_request_queue', false, true, false, false);
+
     // Script waiting for messages
     echo " [*] Waiting for messages from RabbitMQ: mysql_login_response_queue\n";
 
@@ -32,8 +38,35 @@ try {
         echo " [x] Sent login data to RabbitMQ: login_response_queue\n";
     };
 
-    // Consume messages from the RabbitMQ queue
-    $channel->basic_consume('mysql_login_response_queue', '', false, true, false, false, $callback);
+    // This is the backup callback2 function to handle incoming message from login_request_queue and forward to mysql_login_request_queue
+    $callback2 = function($msg) use ($channel) {
+        echo " [x] Backup: Received login Data: ", $msg->body, "\n";
+
+        // Split the received message into email and password
+        $data = explode(",", $msg->body);
+        $email = $data[0];
+        $password = $data[1];
+
+        // Create a new message to send back to RabbitMQ for MySQL login processing
+        $processedMessage = json_encode([
+            'email' => $email,
+            'password' => $password
+        ]);
+
+        // Forward the response to the mysql_login_request_queue
+        $message = new AMQPMessage($processedMessage, ['delivery_mode' => 2]);
+        $channel->basic_publish($message, '', 'mysql_login_request_queue');
+        echo " [x] Backup: Sent login data to RabbitMQ: mysql_login_request_queue\n";
+
+        // Acknowledge the message to rabbitmq
+        $msg->ack();
+    };
+
+    // Consume messages from the queue: mysql_login_response_queue
+    $channel->basic_consume('mysql_login_response_queue', '', false, true, false, false, $callback, null, ['x-priority' => ['I', 2]]); //consume messages from this file first - set as higher priority
+
+    // Consume messages from the queue: login_request_queue (backup for backend1)
+    $channel->basic_consume('login_request_queue', '', false, false, false, false, $callback2, null, ['x-priority' => ['I', 1]]); //and this has lower priority since its the backup
 
     // Keep the script running to listen for incoming messages
     while ($channel->is_consuming()) {
